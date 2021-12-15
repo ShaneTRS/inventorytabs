8a365c87b48363c39150bf1b539c900970347ea1
Shane <80662335+ShaneTRS@users.noreply.github.com>
2021-12-15 15:00:42 -0700

    Introduced keybinds for navigating tabs with the movement keys

--- src/main/java/com/kqp/inventorytabs/init/InventoryTabsClient.java
+++ src/main/java/com/kqp/inventorytabs/init/InventoryTabsClient.java
@@ -14,7 +14,13 @@ import net.minecraft.client.util.InputUtil;
 
 public class InventoryTabsClient implements ClientModInitializer {
     public static final KeyBinding NEXT_TAB_KEY_BIND = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "inventorytabs.key.next_tab", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_TAB, "key.categories.inventory"));
+            "inventorytabs.key.next_tab", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_D, "key.categories.inventory"));
+    public static final KeyBinding PREV_TAB_KEY_BIND = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "inventorytabs.key.prev_tab", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_A, "key.categories.inventory"));
+    public static final KeyBinding MUCHNEXT_TAB_KEY_BIND = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "inventorytabs.key.muchnext_tab", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_S, "key.categories.inventory"));
+    public static final KeyBinding MUCHPREV_TAB_KEY_BIND = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "inventorytabs.key.muchprev_tab", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_W, "key.categories.inventory"));
 
     public static boolean serverDoSightCheckFlag = true;
 
