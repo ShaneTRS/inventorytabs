397f3c21629e1beabfe36a4d0b63b42aceda4d47
Shane <80662335+ShaneTRS@users.noreply.github.com>
2021-12-16 11:03:43 -0700

    Make considerably less stupid

--- src/main/java/com/kqp/inventorytabs/tabs/TabManager.java
+++ src/main/java/com/kqp/inventorytabs/tabs/TabManager.java
@@ -137,23 +137,17 @@ public class TabManager {
         }
         if (InventoryTabsClient.PREV_TAB_KEY_BIND.matchesKey(keyCode, scanCode)) {
             int currentTabIndex = tabs.indexOf(currentTab);
-            if (currentTabIndex < tabs.size() + 1) {onTabClick(tabs.get(currentTabIndex - 1));}
+            if (currentTabIndex > 0) {onTabClick(tabs.get(currentTabIndex - 1));}
                 return true;
         }
         if (InventoryTabsClient.MUCHNEXT_TAB_KEY_BIND.matchesKey(keyCode, scanCode)) {
             int currentTabIndex = tabs.indexOf(currentTab);
-            if (currentTabIndex < tabs.size() - 6) {
-                    onTabClick(tabs.get(currentTabIndex + 6));
-                } else {
-                    onTabClick(tabs.get(tabs.size() - 1));
-                }
-
+            if (currentTabIndex < tabs.size() - 6) {onTabClick(tabs.get(currentTabIndex + 6));}
                 return true;
         }
         if (InventoryTabsClient.MUCHPREV_TAB_KEY_BIND.matchesKey(keyCode, scanCode)) {
             int currentTabIndex = tabs.indexOf(currentTab);
-            if (currentTabIndex < 6) {onTabClick(tabs.get(0));}
-            if (currentTabIndex < tabs.size() + 6) {onTabClick(tabs.get(currentTabIndex - 6));}
+            if (currentTabIndex > 6) {onTabClick(tabs.get(currentTabIndex - 6));}
                 return true;
         }
 
